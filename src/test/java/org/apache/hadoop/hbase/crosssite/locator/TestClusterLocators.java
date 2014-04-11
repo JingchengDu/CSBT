0d265e60892fb0d8af9f5efe7ea1453c96acc657
JingchengDu <jingcheng.du@intel.com>
2014-04-11 16:20:46 +0800

    Correct the package name

--- src/test/java/org/apache/hadoop/hbase/crosssite/locator/TestClusterLocators.java
+++ src/test/java/org/apache/hadoop/hbase/crosssite/locator/TestClusterLocators.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.hadoop.hbase.client.crosssite.locator;
+package org.apache.hadoop.hbase.crosssite.locator;
 
 import static org.junit.Assert.fail;
 
